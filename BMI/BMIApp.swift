ea167ad7b13cf4c282216ef701071562ff3111f4
Vladimir Fibe <motiw@icloud.com>
2022-02-16 07:56:33 +0600

    Initial Commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by Vladimir Fibe on 16.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
